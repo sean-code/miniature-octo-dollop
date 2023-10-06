55eece39c2ac48b29875f91aaa4d6c081b5de012
sean-code <ngangajohn536@gmail.com>
2023-10-06 17:58:28 +0300

    Add: Update README

---
+++ lib/db.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+
+declare global {
+
+    var prisma: PrismaClient | undefined;
+};
+
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (ProcessingInstruction.env.NODE_ENV !== "production") globalThis.prisma = db
